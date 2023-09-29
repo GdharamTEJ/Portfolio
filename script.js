3114148412996c2f0fa89eba9f5465185680db56
TEJA <104780299+GdharamTEJ@users.noreply.github.com>
2023-09-29 15:31:04 +0000

    Portfolio

---
+++ script.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".multiple-text", {
+  strings: [
+    "Fullstack Developer",
+    "Ethical Hacker",
+    "Security Analyst",
+    "YouTuber",
+    "Gamer",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
