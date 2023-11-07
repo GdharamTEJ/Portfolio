a3abf250acdefdad9ab0792a232a3a65764fbfc9
GdharamTEJ <dharamtejpioneer@gmail.com>
2023-11-07 11:59:20 +0530

    Portfolio.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*===== Type Writer Animation in HomePage =====*/
 var typed = new Typed(".multiple-text", {
   strings: [
     "Fullstack Developer",
@@ -11,3 +12,32 @@ var typed = new Typed(".multiple-text", {
   backDelay: 1000,
   loop: true,
 });
+
+/*===== Toggle Icon NavBar =====*/
+
+/*===== Scroll Sections Active Link =====*/
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+
+  /*===== Sticky NavBar =====*/
+  let header = document.querySelector("header");
+
+  header.classList.toggle("sticky", window.scrollY > 100);
+};
